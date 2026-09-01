cb80fd412585f54ca72502f47380779d28d0801f
Gwennyie <roniellagwencapuno@gmail.com>
2026-09-02 01:46:56 +0800

    add program delete view

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -74,3 +74,8 @@ class ProgramUpdateView(UpdateView):
     form_class = ProgramForm
     template_name = "program_forms.html"
     success_url = reverse_lazy('program-list')
+
+class ProgramDeleteView(DeleteView):
+    model = Program
+    template_name = "program_del.html"
+    success_url = reverse_lazy('program-list')
